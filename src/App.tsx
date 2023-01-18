20374388b5cc15a89ce9219917e34589e5c15ed4
Andrii-Kuzmenko <andriikuzmenko35@gmail.com>
2023-01-18 21:20:13 +0200

    todo-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { TodoList } from './components/TodoList';
+import { useLocalStorage } from './helpers';
+import { FilterType } from './types/FilterType';
+import { Todo } from './types/Todo';
 
-function App() {
+export const App: React.FC = () => {
+  const [todos, setTodos] = useLocalStorage<Todo[]>('todos', []);
+  const { filter } = useParams();
+
+  const activeTodos: Todo[] = useMemo(() => {
+    return todos.filter((todo: Todo) => !todo.completed);
+  }, [todos]);
+
+  const completedTodos: Todo[] = useMemo(() => {
+    return todos.filter((todo: Todo) => todo.completed);
+  }, [todos]);
+
+  const removeTodo = useCallback((id: number) => {
+    const filteredTodos = todos.filter((todo: Todo) => todo.id !== id);
+
+    setTodos(filteredTodos);
+  }, [todos]);
+
+  const toggleTodo = useCallback((id: number) => {
+    const toggledTodos = todos.map((todo: Todo) => {
+      if (todo.id !== id) {
+        return todo;
+      }
+
+      return ({
+        ...todo,
+        completed: !todo.completed,
+      });
+    });
+
+    setTodos(toggledTodos);
+  }, [todos]);
+
+  const toggleAll = useCallback(() => {
+    const completed = completedTodos.length !== todos.length;
+
+    const toggledTodos = todos.map((todo: Todo) => {
+      return { ...todo, completed };
+    });
+
+    setTodos(toggledTodos);
+  }, [todos]);
+
+  const updateTodo = useCallback((newTitle: string, id: number) => {
+    const updatedTodo = todos.map((todo: Todo) => {
+      if (todo.id !== id) {
+        return todo;
+      }
+
+      return { ...todo, title: newTitle };
+    });
+
+    setTodos(updatedTodo);
+  }, [todos]);
+
+  const filteredTodos: Todo[] = useMemo(() => {
+    switch (filter) {
+      case FilterType.ACTIVE:
+
+        return activeTodos;
+      case FilterType.COMPLETED:
+
+        return completedTodos;
+      default:
+
+        return todos;
+    }
+  }, [todos, filter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp">
+      <Header todos={todos} setTodos={setTodos} />
+
+      {!todos.length
+        || (
+          <>
+            <TodoList
+              todos={filteredTodos}
+              removeTodo={removeTodo}
+              completedTodos={completedTodos}
+              toggleTodo={toggleTodo}
+              toggleAll={toggleAll}
+              updateTodo={updateTodo}
+            />
+
+            <Footer
+              activeTodos={activeTodos}
+              completedTodos={completedTodos}
+              setTodos={setTodos}
+            />
+          </>
+        )}
     </div>
   );
-}
-
-export default App;
+};
